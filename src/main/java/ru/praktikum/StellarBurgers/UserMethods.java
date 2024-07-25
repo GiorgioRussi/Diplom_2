e271d02af514a6972c36b83109a60ca6bbbe70bb
GiorgioRussi <shonetod7775@gmail.com>
2024-07-25 19:21:39 +0300

    Adding w/o and w/ token user methods.

--- src/main/java/ru/praktikum/StellarBurgers/UserMethods.java
+++ src/main/java/ru/praktikum/StellarBurgers/UserMethods.java
@@ -23,5 +23,56 @@ public class UserMethods extends Constants {
                 .post("/api/auth/login");
     }
 
+    @Step("Получение информации о пользователе с токеном")
+    public Response getInfoWithToken(String token){
+        return given()
+                .baseUri(BASE_URL)
+                .header("Content-type", "application/json")
+                .header("Authorization", token)
+                .get("/api/auth/user");
+    }
+
+    @Step("Получение информации о пользователе без токена")
+    public Response getInfoWithoutToken(){
+        return given()
+                .baseUri(BASE_URL)
+                .header("Content-type", "application/json")
+                .get("/api/auth/user");
+    }
+
+    @Step("Изменение данных пользователя с токеном")
+    public Response changeInfoWithToken(User user, String token){
+        return given()
+                .baseUri(BASE_URL)
+                .header("Content-type", "application/json")
+                .header("Authorization", token)
+                .body(user)
+                .patch("/api/auth/user");
+    }
+    @Step("Изменение данных пользователя без токена")
+    public Response changeInfoWithoutToken(User user) {
+        return given()
+                .baseUri(BASE_URL)
+                .header("Content-type", "application/json")
+                .body(user)
+                .patch("/api/auth/user");
+    }
+
+    @Step("Получение заказов пользователя с токеном")
+    public Response getOrderListWithToken(String token) {
+        return given()
+                .baseUri(BASE_URL)
+                .header("Content-type", "application/json")
+                .header("Authorization", token)
+                .get("/api/orders");
+    }
+
+    @Step("Получение заказов пользователя без токена")
+    public Response getOrderListWithoutToken() {
+        return given()
+                .baseUri(BASE_URL)
+                .header("Content-type", "application/json")
+                .get("/api/orders");
+    }
 
 }
