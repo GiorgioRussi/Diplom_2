c42d55c18189279c0a12276f6cca0a2f6c73e898
GiorgioRussi <shonetod7775@gmail.com>
2024-07-25 18:56:42 +0300

    Adding w/o and w/ token creating orders

--- src/main/java/ru/praktikum/StellarBurgers/OrderMethods.java
+++ src/main/java/ru/praktikum/StellarBurgers/OrderMethods.java
@@ -5,14 +5,31 @@ import io.qameta.allure.Step;
 
 public class OrderMethods extends Constants {
 
-    @Step("Создание заказа")
-    public Response create (Order order){
+    @Step("Создание заказа с токеном")
+    public Response createWithToken(Order order, String token) {
+        return given()
+                .baseUri(BASE_URL)
+                .header("Content-type", "application/json")
+                .header("Authorization", token)
+                .body(order)
+                .post("/api/orders");
+    }
+
+    @Step("Создание заказа без токена")
+    public Response createWithoutToken(Order order) {
         return given()
                 .baseUri(BASE_URL)
                 .header("Content-type", "application/json")
                 .body(order)
                 .post("/api/orders");
+
     }
 
-
+    @Step("Получение списка ингридиентов")
+    public Response getIngredientList() {
+        return given()
+                .baseUri(BASE_URL)
+                .header("Content-type", "application/json")
+                .get("/api/ingredients");
+    }
 }
